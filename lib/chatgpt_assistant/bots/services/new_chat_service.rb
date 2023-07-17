c1eb87a506840957e2605f9e1abdb1ea510154c0
Jesus Gautamah <lima.jesuscc@gmail.com>
2023-07-17 03:04:18 -0300

    adds actors configuration to telegram and chatter (#71)
    * adds actors configuration to telegram and chatter
    
    * refactor new_chat_service

--- lib/chatgpt_assistant/bots/services/new_chat_service.rb
+++ lib/chatgpt_assistant/bots/services/new_chat_service.rb
@@ -14,13 +14,38 @@ module ChatgptAssistant
       TelegramBot.new(@config)
     end
 
+    def actors
+      @actors ||= AwesomeChatgptActors::CastControl.actors
+    end
+
+    def actor
+      @actor_name = actors[@actor_mode.to_i - 1] if @actor_mode
+      @actor = AwesomeChatgptActors::Actor.new(role: @actor_name, language: @config.language) if @actor_name
+    end
+
+    def chat
+      @chat = Chat.new(title: @chat_title, user_id: @user_id, status: 0, actor: @actor_name, prompt: @actor.prompt) if !@actor.nil? && @actor_name
+      @chat = Chat.new(title: @chat_title, user_id: @user_id, status: 0) unless @actor_name
+    end
+
+    def parse_title
+      return @chat_title.strip! unless @chat_title.include?("actor:")
+
+      @actor_mode = @chat_title.split("actor:")[1].strip
+      @chat_title = @chat_title.split("actor:")[0].strip
+    end
+
     def call
-      raise ChatAlreadyExistsError if Chat.find_by(title: @chat_title)
       raise NilError if @chat_title.nil?
       raise NilError if @user_id.nil?
       raise NilError if @chat_id.nil?
 
-      chat = Chat.new(title: @chat_title, user_id: @user_id)
+      parse_title
+
+      raise InvalidModeError unless (@actor_mode.to_i >= 1 && @actor_mode.to_i <= actors.size + 1) || @actor_mode.nil?
+      raise InvalidChatTitleError if @chat_title.nil? || @chat_title.empty?
+      raise ChatAlreadyExistsError if Chat.find_by(title: @chat_title)
+
       if chat.save
         telegram_async.send_message("Chat created succesfully", @chat_id)
       else
